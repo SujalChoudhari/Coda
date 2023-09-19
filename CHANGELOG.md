c42b4d49446e7b7d27c5a89f6dfda5ff6b66f0ad
Sujal Choudhari <sjlchoudhari@gmail.com>
2023-09-19 15:48:53 +0530

    Replaced FileSystem with OS
    Integrated coda utility features with project

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,7 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [0.3.0] - 18-09-2023
 
 ### Added 
-- `FileSystem` package.
+- `OS` package.
 
 ### Changed
 - `import` statements will work in `repl` environment.
--- dll/OS/FileSystem.cpp
+++ dll/OS/FileSystem.cpp
@@ -101,25 +101,93 @@ extern "C" EXPORT void coda_rename(IValuePtr res, IValuePtr args, IEnvironment *
 
 }
 
-
 extern "C" EXPORT void coda_list_files(IValuePtr res, IValuePtr args, IEnvironment * env) {
 	const std::string dirPath = args->getProperties()["dirPath"]->getValue();
 	std::vector<std::string> fileList;
 
-	// Use std::filesystem to list files and subdirectories
+	// Use std::filesystem to list files and subdirectories in the specified directory
 	for (const auto& entry : std::filesystem::directory_iterator(dirPath)) {
-		fileList.push_back(entry.path().string());
+		fileList.push_back(entry.path().filename().string()); // Use filename() to get relative paths
 	}
 
 	auto resProps = res->getProperties();
-	unsigned int count = 1;
+	unsigned int count = 0;
 	for (auto item : fileList) {
-		resProps.insert({ std::to_string(count),res->createNewValue(Coda::Runtime::Type::STRING,item) });
+		resProps.insert({ std::to_string(count), res->createNewValue(Coda::Runtime::Type::STRING, item) });
 		count++;
 	}
 
 	res->setType(Coda::Runtime::Type::LIST);
 	res->setProperties(resProps);
+}
+
+
+
+// Function to read the content of a file
+extern "C" EXPORT void coda_read_file(IValuePtr res, IValuePtr args, IEnvironment * env) {
+	const std::string filePath = args->getProperties()["filePath"]->getValue();
+	std::ifstream inFile(filePath);
+
+	if (inFile.is_open()) {
+		std::string content((std::istreambuf_iterator<char>(inFile)),
+			(std::istreambuf_iterator<char>()));
+		inFile.close();
+
+		res->setType(Coda::Runtime::Type::STRING);
+		res->setValue(content);
+	}
+	else {
+		res->setType(Coda::Runtime::Type::STRING);
+		res->setValue(""); // Return an empty string for file not found or unable to open
+	}
+}
+
+// Function to copy a file from one directory to another
+extern "C" EXPORT void coda_copy_file(IValuePtr res, IValuePtr args, IEnvironment * env) {
+	const std::string sourcePath = args->getProperties()["sourcePath"]->getValue();
+	const std::string destinationPath = args->getProperties()["destinationPath"]->getValue();
+
+	// Use std::filesystem to copy the file
+	try {
+		std::filesystem::copy(sourcePath, destinationPath, std::filesystem::copy_options::overwrite_existing);
+		res->setType(Coda::Runtime::Type::BOOL);
+		res->setValue("1"); // Copy success
+	}
+	catch (...) {
+		res->setType(Coda::Runtime::Type::BOOL);
+		res->setValue("0"); // Copy failed
+	}
+}
+
+// Function to check if a path (file or directory) exists
+extern "C" EXPORT void coda_path_exists(IValuePtr res, IValuePtr args, IEnvironment * env) {
+	const std::string path = args->getProperties()["path"]->getValue();
+
+	// Use std::filesystem to check if the path exists
+	if (std::filesystem::exists(path)) {
+		res->setType(Coda::Runtime::Type::BOOL);
+		res->setValue("1"); // Path exists
+	}
+	else {
+		res->setType(Coda::Runtime::Type::BOOL);
+		res->setValue("0"); // Path does not exist
+	}
+}
+
+// Function to get the size (in bytes) of a file
+extern "C" EXPORT void coda_get_file_size(IValuePtr res, IValuePtr args, IEnvironment * env) {
+	const std::string filePath = args->getProperties()["filePath"]->getValue();
+
+	// Use std::filesystem to get the file size
+	try {
+		std::uintmax_t fileSize = std::filesystem::file_size(filePath);
+		res->setType(Coda::Runtime::Type::INT);
+		res->setValue(std::to_string(fileSize));
+	}
+	catch (...) {
+		res->setType(Coda::Runtime::Type::INT);
+		res->setValue("-1"); // Error in getting file size
+	}
 }
 
 
