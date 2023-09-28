1038293c72b24ba71fa07aa72a50ba76ce412de2
yuvrxj <yuvrajsisodiya7302@gmail,com>
2023-09-28 11:36:17 +0530

    random patch #2

--- dll/Random/Random.cpp
+++ dll/Random/Random.cpp
@@ -1,6 +1,8 @@
 #include "../../FFI/FFI.h"
-#include <cstdlib> // For rand() and srand()
+#include <cstdlib> // For rand_r() and srand()
 #include <ctime>   // For srand() seed
+
+unsigned int seed;
 
 // Seed the random number generator (usually done once at the start of your program)
 void coda_setup()
@@ -8,7 +10,8 @@ void coda_setup()
     static bool seeded = false;
     if (!seeded)
     {
-        srand(static_cast<unsigned>(time(nullptr)));
+        unsigned int seed = static_cast<unsigned int>(time(nullptr))^ static_cast<unsigned int>(pthread_self());
+        srand(seed);
         seeded = true;
     }
 }
@@ -16,7 +19,7 @@ void coda_setup()
 extern "C" EXPORT void coda_random_float(IValuePtr res, IValuePtr args, IEnvironment *env)
 {
     // Generate a random floating-point number in [0, 1)
-    double random_number = static_cast<double>(rand()) / RAND_MAX;
+    double random_number = static_cast<double>(rand_r(&seed)) / RAND_MAX;
 
     res->setType(Coda::Runtime::Type::FLOAT);
     res->setValue(std::to_string(random_number));
@@ -25,7 +28,7 @@ extern "C" EXPORT void coda_random_float(IValuePtr res, IValuePtr args, IEnviron
 extern "C" EXPORT void coda_random_int(IValuePtr res, IValuePtr args, IEnvironment *env)
 {
     // Generate a random integer
-    int random_integer = rand();
+    int random_integer = rand_r(&seed);
 
     res->setType(Coda::Runtime::Type::INT);
     res->setValue(std::to_string(random_integer));
@@ -34,7 +37,7 @@ extern "C" EXPORT void coda_random_int(IValuePtr res, IValuePtr args, IEnvironme
 extern "C" EXPORT void coda_random_long(IValuePtr res, IValuePtr args, IEnvironment *env)
 {
     // Generate a random long integer
-    long random_long = static_cast<long>(rand());
+    long random_long = static_cast<long>(rand_r(&seed));
 
     res->setType(Coda::Runtime::Type::LONG);
     res->setValue(std::to_string(random_long));
